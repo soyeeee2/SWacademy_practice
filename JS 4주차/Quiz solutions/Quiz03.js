440efb4461fdaa783e5f5db13ddf0440d4acfdb9
soyeeee2 <soyeeee2@gmail.com>
2023-01-12 17:52:59 +0900

    todo start

--- JS 4주차/Quiz solutions/Quiz03.js
+++ JS 4주차/Quiz solutions/Quiz03.js
@@ -12,6 +12,6 @@ var idiots = {
     }
 }
 
-idiots.members.roto.play() //band undefined roto play start
+// idiots.members.roto.play() //band undefined roto play start
 
 // * this는 가장 가까운 객체를 가리킴 따라서 roto안의 memberName은 출력, name은 미출력
